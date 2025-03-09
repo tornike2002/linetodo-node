571d4a58a8a947af7343bad63218dab29f642150
tornike2002 <toko.toka2000@gmail.com>
2025-03-09 23:18:20 +0400

    add route for tasks GET and POST req

---
+++ routes/tasks.js
@@ -0,0 +1,29 @@
+export function handleTasksRoutes(req, res) {
+  if (req.method === "GET" && req.url === "/tasks") {
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+      "Cache-Control": "no-cache",
+    });
+    res.end(JSON.stringify({ message: "Tasks fetched successfully" }));
+  } else if (req.method === "POST" && req.url === "/tasks") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      const newTask = JSON.parse(body);
+      res.writeHead(201, {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
+      });
+      res.end(
+        JSON.stringify({ message: "Task created successfully", task: newTask })
+      );
+    });
+  } else {
+    res.writeHead(404, {
+      "Content-Type": "application/json",
+    });
+    res.end(JSON.stringify({ message: "Route not found" }));
+  }
+}
--- server.js
+++ server.js
@@ -6,8 +6,10 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World");
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+  });
+  res.end(JSON.stringify({ message: "Server is running on port " + port }));
 });
 
 server.listen(port, () => {
