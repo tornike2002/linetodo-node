c6d15485654734bd79c2dacef2ef6a0043e226f7
tornike2002 <toko.toka2000@gmail.com>
2025-03-10 23:34:11 +0400

    added my tasks in mongoDB and getting my tasks from mongoDB

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,23 +1,32 @@
-export function handleTasksRoutes(req, res) {
+import { getDB } from "../db/db.js";
+
+export async function handleTasksRoutes(req, res) {
+  const db = await getDB();
+  const tasksCollection = db.collection("tasks");
   if (req.method === "GET" && req.url === "/tasks") {
+    const tasks = await tasksCollection.find({}).toArray();
     res.writeHead(200, {
       "Content-Type": "application/json",
       "Cache-Control": "no-cache",
     });
-    res.end(JSON.stringify({ message: "Tasks fetched successfully" }));
+    res.end(JSON.stringify(tasks));
   } else if (req.method === "POST" && req.url === "/tasks") {
     let body = "";
     req.on("data", (chunck) => {
       body += chunck;
     });
-    req.on("end", () => {
+    req.on("end", async () => {
       const newTask = JSON.parse(body);
+      const result = await tasksCollection.insertOne(newTask);
       res.writeHead(201, {
         "Content-Type": "application/json",
         "Cache-Control": "no-cache",
       });
       res.end(
-        JSON.stringify({ message: "Task created successfully", task: newTask })
+        JSON.stringify({
+          message: "Task created successfully",
+          taskID: result.insertedId,
+        })
       );
     });
   } else {
