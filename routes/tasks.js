00fbaaef91f56c93fa4aba7c4dca94b26ec217b9
tornike2002 <toko.toka2000@gmail.com>
2025-03-13 15:55:35 +0400

    fixed delete request

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import { getDB } from "../db/db.js";
 
 export async function handleTasksRoutes(req, res) {
@@ -28,6 +29,64 @@ export async function handleTasksRoutes(req, res) {
           taskID: result.insertedId,
         })
       );
+    });
+  } else if (req.method.startsWith("DELETE") && req.url.startsWith("/tasks/")) {
+    const taskId = req.url.split("/tasks/")[1];
+    if (!ObjectId.isValid(taskId)) {
+      res.writeHead(400, {
+        "Content-Type": "application/json",
+      });
+      res.end(JSON.stringify({ message: "Invalid task ID" }));
+      return;
+    }
+
+    const result = await tasksCollection.deleteOne({
+      _id: new ObjectId(taskId),
+    });
+
+    if (result.deletedCount > 0) {
+      res.writeHead(200, {
+        "Content-Type": "application/json",
+      });
+      res.end(JSON.stringify({ message: "Task deleted successfully" }));
+      return;
+    }
+    res.writeHead(404, {
+      "Content-Type": "application/json",
+    });
+    res.end(JSON.stringify({ message: "Task not found" }));
+  } else if (req.method.startsWith("PATCH") && req.url.startsWith("/tasks/")) {
+    const taskId = req.url.split("/")[2];
+    if (!ObjectId.isValid(taskId)) {
+      res.writeHead(400, {
+        "Content-Type": "application/json",
+      });
+      res.end(JSON.stringify({ message: "Invalid task ID" }));
+
+      return;
+    }
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", async () => {
+      const updates = JSON.parse(body);
+      const result = await tasksCollection.updateOne(
+        { _id: new ObjectId(taskId) },
+        { $set: updates }
+      );
+      if (result.matchedCount === 0) {
+        res.writeHead(404, {
+          "Content-Type": "application/json",
+        });
+        res.end(JSON.stringify({ message: "Task not found" }));
+        return;
+      } else {
+        res.writeHead(200, {
+          "Content-Type": "application/json",
+        });
+        res.end(JSON.stringify({ message: "Task updated successfully" }));
+      }
     });
   } else {
     res.writeHead(404, {
